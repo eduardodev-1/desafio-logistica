93395be29d7436e5ea4de795023cd4e5f39aaf44
eduardodev-1 <luizeduardodeassis@gmail.com>
2024-10-29 17:40:15 -0300

    bugfix

--- src/main/java/com/luizalabs/logistica/desafio/infra/exception/EmptyFileException.java
+++
@@ -1,7 +0,0 @@
-package com.luizalabs.logistica.desafio.infra.exception;
-
-public class EmptyFileException  extends RuntimeException {
-    public EmptyFileException(String errorMessage) {
-        super("Arquivo vazio: '" + errorMessage + "'");
-    }
-}
